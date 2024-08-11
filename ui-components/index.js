ddf720126d2a8b64041b4e0174b6f45120f4f6c2
jackmuva <33766184+jackmuva@users.noreply.github.com>
2024-08-11 02:20:05 -0700

    no longer need autogenerated forms

--- ui-components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as TasksCreateForm } from "./TasksCreateForm";
-export { default as TasksUpdateForm } from "./TasksUpdateForm";
-export { default as TaskLogsCreateForm } from "./TaskLogsCreateForm";
-export { default as TaskLogsUpdateForm } from "./TaskLogsUpdateForm";
